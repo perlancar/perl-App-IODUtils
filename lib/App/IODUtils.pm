3a185c91be9a4fef28d6b5ebe25928bc2afe72eb
Steven Haryanto (on PC, Jakarta) <stevenharyanto@gmail.com>
2014-08-27 15:11:10 +0700

    Initial commit

---
+++ lib/App/IODUtils.pm
@@ -0,0 +1,19 @@
+package App::IODUtils;
+
+# VERSION
+# DATE
+
+use 5.010001;
+1;
+# ABSTRACT: IOD utilities
+
+=head1 SYNOPSIS
+
+This distribution provides the following command-line utilities:
+
+ dump-iod
+ get-iod-key
+ get-iod-section
+ list-iod-sections
+
+
