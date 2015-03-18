e76e06d21e9102735be1f61065f597ec0e61da3d
perlancar (@pc-office) <perlancar@gmail.com>
2015-03-18 22:04:20 +0700

    Add script: insert-iod-key

--- lib/App/IODUtils.pm
+++ lib/App/IODUtils.pm
@@ -11,12 +11,25 @@ our %common_args = (
         schema  => ['str*'],
         req     => 1,
         pos     => 0,
-        cmdline_src => 'stdin_or_files',
+        cmdline_src => 'stdin_or_file',
     },
     enable_expr => {
         schema  => 'bool',
         default => 0,
         cmdline_aliases => {e=>{}},
+    },
+);
+
+our %inplace_arg = (
+    inplace => {
+        summary => 'Modify file in-place',
+        schema => ['bool', is=>1],
+        description => <<'_',
+
+Note that this can only be done if you specify an actual file and not STDIN.
+Otherwise, an error will be thrown.
+
+_
     },
 );
 
