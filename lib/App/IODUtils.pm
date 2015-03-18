2b883f984ceb4ee7c2c61a0251e2fafc2df6dc44
perlancar (@pc-office) <perlancar@gmail.com>
2015-03-18 22:15:31 +0700

    Add script: insert-iod-key

--- lib/App/IODUtils.pm
+++ lib/App/IODUtils.pm
@@ -33,6 +33,29 @@ _
     },
 );
 
+sub _check_inplace {
+    my $args = shift;
+    if ($args->{inplace}) {
+        die [412, "To use in-place editing, please supply an actual file"]
+            if @{ $args->{-cmdline_srcfilenames_iod} // []} == 0;
+        die [412, "To use in-place editing, please supply only one file"]
+            if @{ $args->{-cmdline_srcfilenames_iod} // []} > 1;
+    }
+}
+
+sub _return_mod_result {
+    my ($args, $doc) = @_;
+
+    if ($args->{inplace}) {
+        require File::Slurper;
+        File::Slurper::write_text(
+            $args->{-cmdline_srcfilenames_iod}[0], $doc->as_string);
+        [200, "OK"];
+    } else {
+        [200, "OK", $doc->as_string, {'cmdline.skip_format'=>1}];
+    }
+}
+
 1;
 # ABSTRACT: IOD utilities
 
